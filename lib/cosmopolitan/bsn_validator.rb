6e2149126e638e8b200b55623be48923869f2528
tomczak-mateusz <mateusz.tomczak@sperasoft.com>
2023-01-21 13:44:58 +0100

    [1] initialize Cosmopolitan::BsnValidator gem - include Ruby 3.1.2, RSpec, GitHub Actions

---
+++ lib/cosmopolitan/bsn_validator.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "bsn_validator/version"
+
+module Cosmopolitan
+  module BsnValidator
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
